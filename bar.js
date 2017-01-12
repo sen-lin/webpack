20c64ec30734713680f29db574d0d7e45df4a2b7
sen-lin <1282694551@qq.com>
2017-01-12 21:41:24 +0800

    add

--- bar.js
+++
@@ -1,7 +0,0 @@
-export default function bar() {
-    var p = document.createElement('p');
-    var str = '白日依山尽，黄河入海流。欲穷千里目，更上一层楼。';
-    var text = document.createTextNode(str);
-	    p.appendChild(text);
-	    document.body.appendChild(p);
-}
--- step-1/bundle.js
+++ step-1/bundle.js
@@ -65,11 +65,13 @@
 	});
 	exports.default = bar;
 	function bar() {
-	    var p = document.createElement('p');
-	    var str = '白日依山尽，黄河入海流。欲穷千里目，更上一层楼。';
-	    var text = document.createTextNode(str);
-	    p.appendChild(text);
-	    document.body.appendChild(p);
+	    // var p = document.createElement('p');
+	    // var str = '白日依山尽，黄河入海流。欲穷千里目，更上一层楼。';
+	    // var text = document.createTextNode(str);
+	    //  p.appendChild(text);
+	    //  document.body.appendChild(p);
+
+	    alert('hello webpack');
 	}
 
 /***/ }
